c9f0db4c432d614f3cdb1034850305e2abc5fdef
Marcelo Ferranti <mferranti@Mac-de-Marcelo.local>
2018-06-18 01:15:28 -0300

    Support to webchat integrator

--- server.js
+++ server.js
@@ -3,7 +3,101 @@ const path = require('path');
 const serveStatic = require('serve-static');
 const socket= require('socket.io');
 const history = require('connect-history-api-fallback');
+const superagent = require('superagent')
+
+const APP_NAME = 'vue';
+const INTEGRATOR_BASE = 'https://awebchat-integration.herokuapp.com';
+const postToIntegrator = async (body, endpoint) => superagent.post(`${INTEGRATOR_BASE}${endpoint}`)
+  .send(body)
+  .set('Accept', 'application/json')
+
+function response(res, statusCode, value) {
+  res.status(statusCode);
+  res.json(value);
+}
+
+function receiveMessage(envelope, target) {
+  return {
+    target,
+    nick: envelope.from.name,
+    message: envelope.msg
+  };
+}
+
+function buildExternalPrivateMessage(envelope, targetApp) {
+  return {
+    ...buildExternalPublicMessage(envelope),
+    to: {
+      id: `id-${envelope.target}`,
+      name: envelope.target,
+    },
+    targetApp,
+  }
+}
+function buildExternalMessage(envelope) {
+  return {
+    from: {
+      id: `id-${envelope.nick}`,
+      name: envelope.nick,
+    },
+    msg: envelope.message,
+    to: null,
+    sourceApp: APP_NAME,
+    targetApp: null,
+    attachment: null,
+  };
+}
+
+function _sendPrivate(envelop) {
+  const target = users.get(envelop.target);
+  const source = users.get(envelop.nick);
+
+  Array.from(new Set([
+    ...(source ? source.sockets : []),
+    ...(target ? target.sockets : []),
+  ])).map(socketId => io.to(socketId).emit('chat', envelop));
+  console.log(`Private message to ${envelop.target}`, envelop);
+}
+
+function _sendPublic(envelop) {
+  io.emit('chat', envelop);
+  console.log('Global message: ', envelop);
+}
+
+function registerExternalUser(envelop) {
+  users.set(envelop.from.name, {sockets: [], app: envelop.sourceApp});
+}
+
 let app = express();
+
+let users = new Map;
+
+app.get('/api/contacts', (req, res) => {
+  const _users = Array.from(users.keys())
+  response(res, 200, _users.map(u => ({id: `id-${u}`, name: u})))
+});
+
+app.post('/api/public/send', (req, res) => {
+  try {
+    const envelop = receiveMessage(req.body, 'global');
+    _sendPublic(envelop);
+    registerExternalUser(envelop);
+    response(res, 200, {});
+  } catch (err) {
+    response(res, err.status, {msg: err.message});
+  }
+});
+
+app.post('/api/private/send', (req, res) => {
+  try {
+    const envelop = receiveMessage(req.body, req.body.to.name);
+    _sendPrivate(envelop);
+    registerExternalUser(envelop);
+    response(res, 200, {});
+  } catch (err) {
+    response(res, err.status, {msg: err.message});
+  }
+});
 
 app.use(history());
 app.use(serveStatic(__dirname + "/dist"));
@@ -15,26 +109,38 @@ let server = app.listen(port, () => {
 });
 
 const io = socket(server);
-let users = new Map;
 io.on('connection', (socket) => {
   console.log('made socket connection', socket.id);
   socket.on('connected', function(data) {
-    const value = users.has(data.nick) ? [...users.get(data.nick), socket.id] : [socket.id];
+    let value;
+    if (users.has(data.nick)) {
+      value = {
+        sockets: [...((users.get(data.nick)).sockets), socket.id],
+        app: APP_NAME,
+      };
+    } else {
+      value = {
+        sockets: [socket.id],
+        app: 'vue',
+      };
+    }
     users.set(data.nick, value);
   });
   socket.on('disconnect', function(data) {
     console.log('disconnect: ', data);
     users = new Map([...users.entries()].map(([nick, sockets]) => [nick, sockets.filter(s => s !== socket.id)]));
   });
-  socket.on('chat', function(data) {
+  socket.on('chat', async (data) => {
     if (data.target === 'global') {
-      io.emit('chat', data);
-      console.log('Global message: ', data);
+      _sendPublic(data);
+      await postToIntegrator(buildExternalMessage(data), '/public/send');
     } else {
-      const targetSockets = users.get(data.target) || [];
-      const sourceSockets = users.get(data.nick) || [];
-      Array.from(new Set([...sourceSockets, ...targetSockets])).map(socketId => io.to(socketId).emit('chat', data));
-      console.log(`Private message to ${data.target}`, data);
+      _sendPrivate(data);
+      if ((users.get(data.target)).app !== APP_NAME) {
+        await postToIntegrator(buildExternalMessage(data, data.target), '/private/send');
+      }
     }
   });
 });
+
+
